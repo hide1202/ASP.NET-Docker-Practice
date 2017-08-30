7880115b1c9b55f6e09f01b8ab2f607a436af796
Taegyeong Kim <hide1202@naver.com>
2017-08-31 00:28:50 +0900

    Remove tag in base docker

--- build-base.sh
+++ build-base.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker login
-docker build -f Dockerfile-Base -t hide1202/aspnetcore-node:0.1 .
-docker push hide1202/aspnetcore-node:0.1
+docker build -f Dockerfile-Base -t hide1202/aspnetcore-node .
+docker push hide1202/aspnetcore-node
